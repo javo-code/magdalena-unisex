8fc3e7e14a35f08aea0d296317628fb33469fec3
Romero Javier <romerofj26@gmail.com>
2024-08-24 03:59:29 -0300

    password - bcryptjs added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,28 @@
 
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 export const register = async (req, res) => {
     const { userName, email, password } = req.body;
 
     try {
+
+        const paswwordHash = await bcrypt.hash(password, 10);
+
         const newUser = new User({
             userName,
             email,
-            password
+            password: paswwordHash,
         });
-        console.log(newUser);
+        //console.log(newUser);
         const userSaved = await newUser.save();
-        res.send(userSaved);
+        res.send({
+            id: userSaved._id,
+            userName: userSaved.userName,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
+
+        });
     } catch (error) {
         console.log(error);
     }
