352160b27665df0ff9ed687595e2e48114518f9d
Romero Javier <romerofj26@gmail.com>
2024-08-24 20:56:27 -0300

    bcrypt + jwt + login added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,33 +1,65 @@
 
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import { createAccesToken } from "../libs/jwt.js";
+
+
 export const register = async (req, res) => {
     const { userName, email, password } = req.body;
 
     try {
-
         const paswwordHash = await bcrypt.hash(password, 10);
 
         const newUser = new User({
             userName,
             email,
-            password: paswwordHash,
+            password: paswwordHash
         });
-        //console.log(newUser);
+
+        console.log(newUser);
+        
         const userSaved = await newUser.save();
-        res.send({
+
+        const token = await createAccesToken({ id: userSaved._id})
+        res.cookie('token', token);
+        
+        res.json({
             id: userSaved._id,
-            userName: userSaved.userName,
+            user: userSaved.userName,
             email: userSaved.email,
             createdAt: userSaved.createdAt,
-            updatedAt: userSaved.updatedAt,
-
+            updatedeAt: userSaved.updatedAt,
         });
+
     } catch (error) {
         console.log(error);
     }
 }
 
-export const login = (req, res) => {
-    res.send('login')
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+
+        const userFound = await User.findOne({ email });
+        if (!userFound) return res.status(400).json({ message: "User not found" });
+        
+        const isMAtch = await bcrypt.compare(password, userFound.password);
+
+        if (!isMAtch) return res(400).json({ message: 'Incorrect Password' });
+
+        const token = await createAccesToken({ id: userFound._id})
+        res.cookie('token', token);
+        
+        res.json({
+            id: userFound._id,
+            user: userFound.userName,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedeAt: userFound.updatedAt,
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 }
