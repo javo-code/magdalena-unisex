84f7246e0280ff9571600e7b2f350a1a7fa08488
Romero Javier <romerofj26@gmail.com>
2024-09-19 21:46:26 -0300

    register page + react-router-dom + react-hook-form added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+//import { LoginPage } from "./pages/LoginPage";
+//import { RegisterPage } from "./pages/RegisterPage";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>}></Route>
+        <Route path='/login' element={<h1>Login</h1>}></Route>
+        <Route path='/register' element={<h1>Register</h1>}></Route>
+        <Route path='/reports' element={<h1>Reports</h1>}></Route>
+        <Route path='/add-report' element={<h1>New Report</h1>}></Route>
+        <Route path='/report/:id' element={<h1>Update Report</h1>}></Route>
+        <Route path='/profile' element={<h1>Profile</h1>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
+
 
 export default App
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,22 @@
+import { useForm } from "react-hook-form";
+
+function RegisterPage() {
+
+    const { register, handleSubmit } = useForm();
+    
+    return (
+        <form onSubmit={handleSubmit((values) => {
+                console.log(values);
+            })}
+            >
+                <input type="text" {...register("username", { required:true })}/>
+                <input type="email"  {...register("email", { required:true })}/>
+                <input type="password"  {...register("password", { required: true })}/>
+                <button type="submit">
+                    Register
+                </button>
+            </form>
+    )
+}
+
+export default RegisterPage
